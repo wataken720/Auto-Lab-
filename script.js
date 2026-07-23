0946462e489fd3739e88b0724f17f7cbd924dd86
wataken720 <watakenroom@gmail.com>
2026-07-23 12:00:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const players = {
+
+    "青山 周平": {
+        rank: "S級S",
+        time: "3.31",
+        st: "0.08",
+        diff: "+0.08",
+        results: "①①②①③①①②③①"
+    },
+
+    "鈴木 圭一郎": {
+        rank: "S級S",
+        time: "3.28",
+        st: "0.07",
+        diff: "+0.05",
+        results: "①②①①②③①①②①"
+    }
+
+};
+
 function openPlayer(name){
 
     document.getElementById("playerName").innerHTML = "👤 " + name;
