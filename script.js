4f3d04272c780b809f31261dfd73f97d5c7980d8
wataken720 <watakenroom@gmail.com>
2026-07-23 11:50:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ function openPlayer(name){
 
     document.getElementById("playerName").innerHTML = "👤 " + name;
 
+    document.getElementById("playerRank").innerHTML = "S級S";
+
+    document.getElementById("playerTime").innerHTML = "3.31";
+
+    document.getElementById("playerST").innerHTML = "0.08";
+
+    document.getElementById("playerDiff").innerHTML = "+0.08";
+
+    document.getElementById("playerResults").innerHTML = 
+    "①①②①③①①②③①";
+
     document.getElementById("playerModal").style.display="block";
 
 }
